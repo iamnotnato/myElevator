719d3fed6d6b216f67cba7f56f4ae42ca4baf879
Alan Nato <alanwnato@gmail.com>
2023-08-27 13:05:27 +0300

    feat : added mute functionality

--- js/myElevator.js
+++ js/myElevator.js
@@ -6,13 +6,16 @@ button.addEventListener("click", () => {
   if (audio.paused) {
     audio.volume = 0.2;
     audio.play();
-    icon.classList.remove('fa-volume-up');
-    icon.classList.add('fa-volume-mute');
+    icon.classList.remove('fa-solid fa-volume-high');
+    icon.classList.add('fa-solid fa-volume-xmark');
     
   } else {
     audio.pause();
-    icon.classList.remove('fa-volume-mute');
-    icon.classList.add('fa-volume-up');
+    icon.classList.remove('fa-solid fa-volume-xmark');
+    icon.classList.add('fa-solid fa-volume-high');
   }
   button.classList.add("fade");
 });
+
+
+document.getElementById("demo").innerHTML = 'Elevator Requested';
